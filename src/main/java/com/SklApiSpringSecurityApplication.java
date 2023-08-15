2fd844f6df2f9d9e96403082ea332d731fcdc8e0
Vincent Prouchet <prouchetvincent@hotmail.com>
2023-08-15 16:37:51 +0200

    first commit

---
+++ src/main/java/com/SklApiSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklApiSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SklApiSpringSecurityApplication.class, args);
+	}
+
+}
